6510edc23a46061b83f6480cd0e7bdc3d3358bd6
Marc Altmann <marcaltmann@posteo.de>
2024-01-19 17:51:14 +0100

    Extract send_email function

---
+++ email_notifications.py
@@ -0,0 +1,24 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+from app_config import get_config
+
+def send_email(subject, body, type):
+    "Sends email to recipients specified in config."
+    email_config = get_config()['email']
+
+    server = smtplib.SMTP(email_config['smtp_server'], email_config['smtp_port'])
+    server.starttls()
+
+    for recipient in email_config['to']:
+        print(f"==> Sending {type} email to {recipient}.")
+        message = MIMEMultipart('alternative')
+        message['From'] = email_config['from']
+        message['To'] = recipient
+        message['Subject'] = subject
+        message.attach(MIMEText(body, 'html'))
+        text = message.as_string()
+        server.sendmail(email_config['from'], recipient, text)
+
+    server.quit()
