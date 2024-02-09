d03d227708208de0807b2296df44480a5cf88e90
Marc Altmann <marcaltmann@posteo.de>
2024-02-09 12:04:34 +0100

    Remove asr-evaluate stuff

--- asr-evaluate/config_example.py
+++
@@ -1,8 +0,0 @@
-
-#[vtt_cleaning]
-vtt_directory = "/Users/ahenderson/Documents/Whisper_Test_Files/_input/"
-output_directory = "/Users/ahenderson/Documents/Whisper_Test_Files/_output/"
-
-#[wer_calculation]
-reference_directory = "/Users/ahenderson/Documents/Whisper_Test_Files/reference/"
-hypothesis_directory = "/Users/ahenderson/Documents/Whisper_Test_Files/hypothesis/"
